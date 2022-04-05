b9a49208c41d13291f58cf75aa96a1b7326f033a
johan <johan1035.gh@gmail.com>
2022-04-05 18:34:30 -0500

    Add project files.

---
+++ Importaciones_SABS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Importaciones_SABS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
