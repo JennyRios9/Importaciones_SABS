78b8033d2d549e694f4e8ad6babd0b2d6aa0f2e8
Yeny Rios <yeny.rios9@misena.edu.co>
2022-04-06 12:02:01 -0500

    Migracion Sena

---
+++ Importaciones_SABS/Migrations/20220406144406_Migracion Sena.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Importaciones_SABS.Migrations
+{
+    public partial class MigracionSena : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
