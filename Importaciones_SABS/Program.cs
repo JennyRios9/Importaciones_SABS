09f7892acc51a91cc60c2c9dd1186a7e4b493bcf
Yeny Rios <yeny.rios9@misena.edu.co>
2022-04-06 12:02:19 -0500

    Cmbio de servidor Sena

--- Importaciones_SABS/Program.cs
+++ Importaciones_SABS/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSqlServer")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSqlServerSena")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
