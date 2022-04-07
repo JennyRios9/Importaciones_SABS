eb75d155f709264239cf40a9c9e6d6d1622ae11a
johan <johan1035.gh@gmail.com>
2022-04-07 00:20:25 -0500

    vistas de cada modulo

--- Importaciones_SABS/Program.cs
+++ Importaciones_SABS/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSqlServerSena")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSqlServer")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Inicio}/{id?}");
 
 app.Run();
